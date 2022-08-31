93509d1097a319814bd314faa66186ddc4ad3528
YuliiaMatich <yuliia.matich@gmail.com>
2022-08-31 15:22:41 -0400

    add screenshots

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 3, maximum: 8}
 
-  def authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email, password)
     user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       return user
